ff04c859d9908bffd5c4f4ee65c53bddc646c7d1
nesky-rf <jordi.capdevila@gmail.com>
2021-04-16 09:39:56 +0200

    Initial update
    The aim of this project is build an ML model to classify skin moles, as benign or malign. Notebooks explains from gather data images using ISIC API, data exploration, data features, build and compare ML models, evaluate them and deploy the ML model to different platforms.

---
+++ flask_app/app.py
@@ -0,0 +1,98 @@
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+import sqlite3
+
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
+app = Flask(__name__)
+app.config['SECRET_KEY']= "password"
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+    return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+
+@app.route('/<string:id>/predict')#, methods=('POST',))
+def predict(id):
+    #if request.method == 'POST':
+    #title = request.form['title']
+    #content = request.form['content']
+    model = load_model('../models/transfer.h5')
+    input_image = Image.open('../data_kaggle_test/benign/'+id+'.jpg')
+    prediction = model.predict(np.reshape(input_image,(1,224,224,3))/255)
+    return render_template('predict.html',prediction=prediction[0][0])
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
